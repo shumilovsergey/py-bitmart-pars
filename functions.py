9b9a6b80973c99dc288ce2fda932330a5ec4a3ba
shumilov <wumilovsergey@gmail.com>
2023-11-26 19:57:35 +0300

    dockerfile testing

--- functions.py
+++ functions.py
@@ -8,15 +8,19 @@ from const import FILE_PATH
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 def get():
     r = ""
     err = True
     chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox") 
     chrome_options.add_argument("--headless") 
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     try:
         driver.get(URL)
